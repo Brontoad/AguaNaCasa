4fdee564a6a066c455d3a720f09145aca9e910b7
Brontoad <brontoadgames@gmail.com>
2026-05-13 18:25:33 +0000

    Updated main

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,29 +1,42 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
-import { Container, createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import '../css/agua_na_casa.css';
 // import { initializeTheme } from './hooks/use-appearance';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
+console.log("KLSAKLSKAS");
 createInertiaApp({
-    resolve: (name: string) =>
-        resolvePageComponent(
-            `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.{tsx,ts}'),
-        ),
-    setup({ el, App, props }: {el: Container, App: any, props: any}) {
-        const root = createRoot(el);
+    resolve: async (name: string) => {
+        console.log('Requested page:', name);
 
-        root.render(
+        const pages = import.meta.glob('./pages/**/*.tsx');
+
+        console.log('Available pages:', Object.keys(pages));
+
+        const path = `./pages/${name}.tsx`;
+
+        console.log('Resolved path:', path);
+
+        const page = pages[path];
+
+        console.log('Matched page:', page);
+
+        if (!page) {
+            throw new Error(`Page not found: ${path}`);
+        }
+
+        return resolvePageComponent(path, pages);
+    },
+
+    setup({ el, App, props }) {
+        createRoot(el).render(
             <StrictMode>
                 <App {...props} />
-            </StrictMode>,
+            </StrictMode>
         );
-    },
-    progress: {
-        color: '#4B5563',
     },
 });
 
